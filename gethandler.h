a86e20817c4ee5692e3d4ba4a2bbeaf083f8aeda
Guido Winkelmann <guido@unknownsite.de>
2019-12-25 22:53:27 +0100

    Add some more comments explaining methods

--- gethandler.h
+++ gethandler.h
@@ -16,8 +16,19 @@ class GetHandler : public HandlerBase
     virtual uint64_t content_length();
 
     // Override the following if at all possible
+
+    /* Returns the MIME-type of the returned data as a string.
+       The default implementation returns "text/html". */
     virtual std::string content_type();
+
+    /* Returns whether charset() will be able to tell the character
+       encoding.
+       The default implementation returns false */
     virtual bool charset_known();
+
+    /* Returns the charset of the output.
+       Only used if charset_known returns true.
+       Default implementation throws CharsetUnknown */
     virtual std::string charset();
     };
 
--- path.h
+++ path.h
@@ -10,6 +10,9 @@ namespace asio_httpd {
 
 // Supports Unix-style absolute paths only
 // Always normalizes the given path
+// We do not use std::filesystem::path because that has slightly different
+// semantics from what we need for HTTP.
+// TODO: Elaborate on those differences.
 
 class Path
     {
@@ -25,10 +28,17 @@ class Path
     bool operator<(const Path& path) const;
 
     std::string str() const;
+
+    /* Determines if this path is in a subtree of path */
     bool is_in_subtree(const Path& path) const;
+
+    /* If this is a subtree of other_path, return the path components that
+       are not part of other_path.
+       Example: Path("/a/b/c").difference("/a/") returns Path("b/c")
+       Throws NotInSubtree if this is not in a subtree of other_path.*/
+    Path difference(const Path& path) const;
     unsigned int num_elements() const;
     bool is_absolute() const;
-    Path difference(const Path& path) const;
 
     class MalformedPath : public std::runtime_error
         {
